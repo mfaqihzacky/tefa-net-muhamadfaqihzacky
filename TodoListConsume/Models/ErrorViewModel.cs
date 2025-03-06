97d7aaa3ae5d400a841b2daa6f5b0cc770dc0cb7
Muhamad Faqih Zacky <mfaqihzacky@gmail.com>
2025-03-07 00:09:39 +0700

    Menghapus File Yang Tidak Jadi Digunakan

--- TodoListConsume/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TodoListConsume.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
